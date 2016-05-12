1366d8f3ccfc41283be861b9b05b1bcb9ca0092f
Deniz Silahcılar <deniz.silahcilar@PT0001.local>
2016-05-12 11:47:17 +0300

    request scope

--- src/main/java/com/example/service/FirstService.java
+++ src/main/java/com/example/service/FirstService.java
@@ -2,11 +2,14 @@ package com.example.service;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Service;
 
 @Service
+@Scope(scopeName = "request",proxyMode = ScopedProxyMode.TARGET_CLASS)
 @Setter
 @Getter
 public class FirstService {
--- src/main/java/com/example/web/FirstController.java
+++ src/main/java/com/example/web/FirstController.java
@@ -18,6 +18,7 @@ public class FirstController {
 
     @RequestMapping(path = "/")
     public String first() {
+
         System.out.println(firstService);
 
         firstService.setName("deniz");
